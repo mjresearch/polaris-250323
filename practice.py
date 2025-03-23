b2bbc5c3943d34035996af3dc5a7b30abebc3c9b
LucidAwareness <lucidawareness@LucidAwarenessui-MacBookAir.local>
2025-03-23 16:47:31 +0900

    문자열공부

--- practice.py
+++ practice.py
@@ -1,4 +1,22 @@
-from random import *
+# python = "Python is Amazing."
 
-date = randint(4, 28)
-print(f"오프라인 스터디 모임 날짜는 {date}일로 선정되었습니다.")
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper())
+# print(len(python))
+# print(python.replace("Python", "Java"))
+
+# index = python.index("n")
+# print(index)
+# index = python.index("n", index + 1)
+# print(index)
+
+# print(python.find("n"))
+
+# print(python.count("n"))
+
+print("나는 %s살 입니다." % 44)
+print("나는 %s와 %s를 좋아합니다." % ("딸기", "바나나"))
+
+print("나는 {}을 공부합니다.".format("파이썬"))
+print("나는 {}와 {}을 좋아해요.".format("덴빠","덴뿌라"))
