b83dd65b3e262eab99b1639f068f81b5843e4ef9
LucidAwareness <lucidawareness@LucidAwarenessui-MacBookAir.local>
2025-03-29 16:45:51 +0900

    스프린트 python 기초 3 진행 중

--- practice.py
+++ practice.py
@@ -1,22 +1,40 @@
-# python = "Python is Amazing."
+# weather = input("오늘 날씨 어때요? ")
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요.")
+# elif weather == "미세먼지":
+#     print("마스크를 착용하세요.")
+# else:
+#     print("즐거운 하루 되세요.^^") 
 
-# print(python.lower())
-# print(python.upper())
-# print(python[0].isupper())
-# print(len(python))
-# print(python.replace("Python", "Java"))
+# temp = int(input("기온은 어때요? "))
+# if temp >= 30:
+#     print("많이 더워요. 시원한 음료를 챙기세요.")
+# elif temp >= 10 and temp < 30:
+#     print("가벼운 산책이 가능해요.")
+# elif 0 < temp < 10:
+#     print("외투를 꼭 입으세요.")
+# else:
+#     print("추운 날씨입니다. 외출을 삼가세요.")
 
-# index = python.index("n")
-# print(index)
-# index = python.index("n", index + 1)
-# print(index)
 
-# print(python.find("n"))
 
-# print(python.count("n"))
+# for waiting_no in range(1, 6):
+#    # print("대기번호 : {0}".format(waiting_no))
+#     print(f"대기번호 : {waiting_no}")
 
-print("나는 %s살 입니다." % 44)
-print("나는 %s와 %s를 좋아합니다." % ("딸기", "바나나"))
 
-print("나는 {}을 공부합니다.".format("파이썬"))
-print("나는 {}와 {}을 좋아해요.".format("덴빠","덴뿌라"))
+# cafe = ["모규님", "덴빠님", "용수스님", "법상스님"]
+# for customer in cafe:
+#     print(f"{customer}, 주문하신 음료 나왔습니다.")
+
+
+# customer = "모규님"
+# index = 3
+# while index >= 1:
+#     print(f"{customer}, 음료 나왔습니다. {index}번 남았습니다.")
+#     index -= 1
+#     if index == 0:
+#         print("오지 않으셔서 음료는 처분되었습니다.")
+
+
+
